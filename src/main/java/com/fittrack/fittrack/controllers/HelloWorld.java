4a22a2d4c40ca5bb30e5fb3b6c20eb5131ba5192
Isabela Targino <isabela.targino2505@icloud.com>
2026-03-16 20:32:54 -0300

    initial setup for fitTrack

---
+++ src/main/java/com/fittrack/fittrack/controllers/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.fittrack.fittrack.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloWorld {
+
+    @GetMapping
+    public String sayHello() {
+        return "Hello, FitTrack!";
+    }
+}
